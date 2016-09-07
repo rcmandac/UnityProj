85a401795445d15b1551632b86b05481799796f5
CADNAM\RowellChristian <rcmandac>
2016-09-07 21:59:54 +0800

    Scene Change upon Clicking Login Button but no Login Module.

---
+++ New Unity Project/Assets/Scripts/MenuCtrl.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class MenuCtrl : MonoBehaviour {
+
+
+    public void SceneLoad(string scene)
+    {
+        SceneManager.LoadScene(scene);
+    }
+}
